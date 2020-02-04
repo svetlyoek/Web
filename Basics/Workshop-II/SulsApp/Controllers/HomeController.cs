1c5e130dd6b97c47c576564f4d27c30aad1f7145
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-02-04 22:21:46 +0200

    Add new

---
+++ Basics/Workshop-II/SulsApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using SIS.HTTP;
+using SIS.HTTP.Response;
+using SIS.MvcFramework;
+using SulsApp.ViewModels;
+using System;
+
+namespace SulsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            var viewModel = new IndexViewModel
+            {
+                Message = "Welcome to SULS Platform!",
+                Year = DateTime.UtcNow.Year,
+            };
+            return this.View(viewModel);
+        }
+    }
+}
