84e5cc8c5dded71182de200bae4a4cdaabfaf0e9
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-02-08 08:43:47 +0200

    Add new

---
+++ Basics/MVC advanced-ioc and data binding/SIS-IOC and Data Binding/SulsApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using SIS.HTTP;
+using SIS.HTTP.Logging;
+using SIS.HTTP.Response;
+using SIS.MvcFramework;
+using SulsApp.Services;
+using SulsApp.ViewModels.Home;
+using System;
+
+namespace SulsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger logger;
+
+        public HomeController(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            this.logger.Log("Hello from Index");
+            var viewModel = new IndexViewModel
+            {
+                Message = "Welcome to SULS Platform!",
+                Year = DateTime.UtcNow.Year,
+            };
+            return this.View(viewModel);
+        }
+    }
+}
