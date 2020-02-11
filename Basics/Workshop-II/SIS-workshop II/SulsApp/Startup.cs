32a0f0baf9ef32197a973a2bc9b8a6268e67a65c
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-02-11 16:43:15 +0200

    Add new

---
+++ Basics/Workshop-II/SIS-workshop II/SulsApp/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIS.HTTP;
+using SIS.MvcFramework;
+using System.Collections.Generic;
+
+namespace SulsApp
+{
+    public class Startup : IMvcApplication
+    {
+        public void ConfigureServices()
+        {
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
+        }
+
+        public void Configure(IList<Route> routeTable)
+        {
+        }
+    }
+}
