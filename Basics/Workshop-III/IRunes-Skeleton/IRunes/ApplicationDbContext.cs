8f4dcea85df754577f4cec2e6637dfa895b4890e
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-02-13 09:08:27 +0200

    Add new

---
+++ Basics/Workshop-III/IRunes-Skeleton/IRunes/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IRunes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-JH4M4M9\MSSQLSERVER01;Database=IRunes;Integrated Security=True;");
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+    }
+}
